493df4f78c360fce414ad37d866dbc2e5857882e
bot <Krampus.sack.never@gmail.com>
2021-12-16 20:51:25 +0100

    disable lappack

--- src/main/java/com/github/gtmega/baublesbatpacks/items/Items.java
+++ src/main/java/com/github/gtmega/baublesbatpacks/items/Items.java
@@ -22,7 +22,7 @@ public class Items {
         registerBatPack(Item.BatPack,InternalName.itemArmorBatpack,120_000D,1,200D);
         registerBatPack(Item.AdvPack,InternalName.itemArmorAdvBatpack,1_200_000D,2,2_000D);
         registerBatPack(Item.EnergyPack,InternalName.itemArmorEnergypack,4_000_000D,3,8_000D);
-        registerBatPack(Item.LapPack,InternalName.itemArmorLappack,20000000D,4,80_000D);
+        //registerBatPack(Item.LapPack,InternalName.itemArmorLappack,20000000D,4,80_000D);
         Item.AdvancedLapPack.stack = new ItemStack(new AdvancedLapPack(6_000_000,3,1000_000));
         Item.UltimateLapPack.stack = new ItemStack(new UltimateLapPack(60_000_000,4,600_000));
         Item.EpicLapPack.stack = new ItemStack(new EpicLapPack(600_000_000,5,6_000_000));
