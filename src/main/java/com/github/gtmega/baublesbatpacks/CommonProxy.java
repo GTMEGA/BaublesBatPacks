eee2886960982cbe6e6c7884077d43d7b06baf20
matt-159 <16054364+matt-159@users.noreply.github.com>
2023-09-04 19:32:50 -0700

    replace mod check with try catch

--- src/main/java/com/github/gtmega/baublesbatpacks/CommonProxy.java
+++ src/main/java/com/github/gtmega/baublesbatpacks/CommonProxy.java
@@ -23,9 +23,11 @@ public class CommonProxy {
 //        BaubleExpandedSlots.tryRegisterType("batpack-producer");
 //        BaubleExpandedSlots.tryAssignSlotOfType("batpack-producer");
 
-        if (Loader.isModLoaded("Baubles Expanded")) {
+        try {
             BaubleExpandedSlots.tryRegisterType(Constants.batpackType);
             BaubleExpandedSlots.tryAssignSlotOfType(Constants.batpackType);
+        } catch (Exception e) {
+            BaublesBatPacks.warn("Could not register dedicated batpack bauble slot");
         }
 
         Items.init();
